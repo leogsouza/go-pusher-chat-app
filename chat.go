50bf2334c4016ac44ef3d4b4aa8e051421be04c8
Leonardo Souza <leogsouza@gmail.com>
2019-10-20 22:05:18 -0300

    Create chat application

---
+++ chat.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	pusher "github.com/pusher/pusher-http-go"
+)
+
+var client = pusher.Client{
+	AppID:   os.Getenv("PUSHER_APP_ID"),
+	Key:     os.Getenv("PUSHER_APP_KEY"),
+	Secret:  os.Getenv("PUSHER_APP_SECRET"),
+	Cluster: os.Getenv("PUSHER_APP_CLUSTER"),
+	Secure:  true,
+}
+
+type user struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func registerNewUser(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	var newUser user
+
+	err = json.Unmarshal(body, &newUser)
+	if err != nil {
+		panic(err)
+	}
+
+	client.Trigger("update", "new-user", newUser)
+
+	json.NewEncoder(w).Encode(newUser)
+}
+
+func pusherAuth(w http.ResponseWriter, r *http.Request) {
+	params, _ := ioutil.ReadAll(r.Body)
+	response, err := client.AuthenticatePrivateChannel(params)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(response))
+}
+
+func main() {
+
+	port := os.Getenv("port")
+	if port == "" {
+		port = "8090"
+	}
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	http.HandleFunc("/new/user", registerNewUser)
+	http.HandleFunc("/pusher/auth", pusherAuth)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
